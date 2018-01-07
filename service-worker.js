8c3c97766e4ffb15a84c9a67ec7a12cd4fddaca1
Julien Blégean <julien.blegean@gmail.com>
2018-01-07 21:38:46 +0100

    added service worker

--- service-worker.js
+++ service-worker.js
@@ -43,9 +43,5 @@ self.addEventListener('activate', function(e) {
 })
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  return
 })

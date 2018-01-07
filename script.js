8a99b4e251f6de04b6ff1f236688494ae906221d
Julien Blégean <julien.blegean@gmail.com>
2018-01-07 21:38:08 +0100

    added service worker

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function activateCamera() {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
